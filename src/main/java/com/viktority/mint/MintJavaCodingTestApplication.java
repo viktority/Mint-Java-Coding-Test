416e9f59ef47d5e4ab5264bbda6e9ccb4f095c62
Chukwuka Onyekachukwu Victor <viktority@yahoo.com>
2020-03-10 11:25:05 +0100

    first commit

---
+++ src/main/java/com/viktority/mint/MintJavaCodingTestApplication.java
@@ -0,0 +1,13 @@
+package com.viktority.mint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MintJavaCodingTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MintJavaCodingTestApplication.class, args);
+	}
+
+}
